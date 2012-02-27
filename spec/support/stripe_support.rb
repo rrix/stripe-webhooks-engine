c2b8913a19de9a004bfe7be6b517b1f8995f30b1
Ryan Rix <ry@n.rix.si>
2012-02-27 15:43:10 -0700

    T: Load stripe stuff properly

--- spec/support/stripe_support.rb
+++ spec/support/stripe_support.rb
@@ -1,5 +1,5 @@
 def load_webhook_json webhook_name
   path = Dir[File.join(ENGINE_RAILS_ROOT, "spec/data/stripe/#{ webhook_name }.json")]
-  json = File.open(path).read
+  json = File.open(path[0]).read
   JSON.parse json
 end
